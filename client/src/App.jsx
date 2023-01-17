8468bfb08a700b03543561eb3ca49494c4375d33
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-17 19:00:31 +0000

    Scaffolded front end

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="app">
+      <h1>Vite + React</h1>
+    </div>
+  );
+}
+
+export default App;
