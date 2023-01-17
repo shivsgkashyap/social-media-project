713784bd9aa305cef32bfeff496bbadf41bcda7e
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-17 16:54:34 +0000

    Added JWT verification

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -10,6 +10,10 @@ export const verifyToken = async (req, res, next) => {
     if (token.startswith("Bearer ")) {
       token = token.slice(7, token.length).trimLeft();
     }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
