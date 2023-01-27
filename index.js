4bfecfe9dfc11b2c54c5b580a99958438c2e6bbb
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-27 11:17:46 +0000

    Fixing bug

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 app.use(express.static(path.join(__dirname, "client/dist")));
 
 app.get("*", (req, res) =>
-  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
 );
 
 /* FILE STORAGE */
