13aa4fa1b28e52924fe4c24fed487759c40ed471
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-27 10:39:37 +0000

    Fixed bug

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
-const fileDirname = path.dirname(__filename);
+const fileDirname = path.__dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(express.json());
