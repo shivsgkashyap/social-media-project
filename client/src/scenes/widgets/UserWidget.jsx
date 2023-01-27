e01a4ff56756fbf1c69e7be7cfa7b2f0f39bdcee
Shiv Kashyap <89941894+shivsgkashyap@users.noreply.github.com>
2023-01-27 12:27:25 +0000

    Fixing bugs

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -24,10 +24,13 @@ const UserWidget = ({ userId, picturePath }) => {
   const main = palette.neutral.main;
 
   const getUser = async () => {
-    const response = await fetch(`/api/users/${userId}`, {
-      method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
-    });
+    const response = await fetch(
+      `https://silly-hen-tux.cyclic.app/api/users/${userId}`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
     const data = await response.json();
     setUser(data);
   };
